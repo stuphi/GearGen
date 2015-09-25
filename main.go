16c7eeadfa64d38d3c09430a417407f993f6b39b
stuphi <philip@stuphi.co.uk>
2015-09-25 09:46:45 +0100

    Try and calculate our spacing

--- main.go
+++ main.go
@@ -9,10 +9,28 @@ func main() {
 	fmt.Println("This is my gear program")
 	fmt.Println("#######################")
 
-	var myGear gear.Gear
-	myGear.Pd = 100
-	myGear.N = 20
-	myGear.A = 25
+	var Centers float64 // Distance between Centers
+	var Ratio float64 // Required Ratio
+	var DriveTeeth float64 // Number of teeth on drive gear
+	var PressureAngle float64
 
-	fmt.Print(myGear.ToString())
+	Centers = 100.0
+	Ratio = 2.0
+	DriveTeeth = 10
+	PressureAngle = 25
+
+	var Gear1 gear.Gear
+	Gear1.Pd = (1 / (Ratio + 1)) * Centers * 2
+	Gear1.N = DriveTeeth
+	Gear1.A = PressureAngle
+
+	var Gear2 gear.Gear
+	Gear2.Pd = (Ratio / (Ratio + 1)) * Centers * 2
+	Gear2.N = DriveTeeth * Ratio
+	Gear2.A = PressureAngle
+
+	fmt.Print(Gear1.ToString())
+	fmt.Print(Gear2.ToString())
+
+	fmt.Printf("Actual Ratio: %0.4f\n", Gear2.Pd / Gear1.Pd)
 }
