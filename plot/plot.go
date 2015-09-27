d46f007b197d4712e11775e79bf205cb589b9b66
stuphi <philip@stuphi.co.uk>
2015-09-27 14:50:09 +0100

    Start with some involutes

--- plot/plot.go
+++ plot/plot.go
@@ -13,45 +13,65 @@ const (
     DegToRad = math.Pi / 180.0
     RadToGrad = 200.0 / math.Pi
     GradToDeg = math.Pi / 200.0
+    factor = 100
 )
 
 const (
   dashStyle = "fill:none; stroke-width:20; stroke:black; stroke-dasharray:300,100,100,100"
   solidStyle = "fill:none; stroke-width:30; stroke:black"
   thinStyle = "fill:none; stroke-width:10; stroke:black"
-  gridStyle = "fill:none; stroke-width:8; stroke:grey"
+  gridStyle = "fill:none; stroke-width:10; stroke:lightgrey"
 )
 
 func plotGrid(cx int, cy int, width int, height int, canvas *svg.SVG){
-  spaceing := 500 // 5mm
+  spaceing := 5 * factor // 5mm
 
-  canvas.Group()
-  for i:=0; i < cy; i += spaceing {
-    canvas.Line(0, cy + i, width, cy + i, gridStyle)
-    canvas.Line(0, cy - i, width, cy - i, gridStyle)
+  gx := int(math.Mod(float64(cx), float64(spaceing)))
+  gy := int(math.Mod(float64(cy), float64(spaceing)))
+
+  gw := width - (2 * gx)
+  gh := height - (2 * gy)
+
+  canvas.Grid(gx, gy, gw, gh, spaceing, gridStyle)
+}
+
+func plotInvCurve(g gear.Gear, canvas *svg.SVG){
+  var px []int
+  var py []int
+  var xc, yc float64
+  var ang float64
+  var s float64
+  r := g.GetBaseCircleDia() * factor / 2
+  for i:=0.0; i<1; i = i + 0.05{
+    ang = i * 90 * DegToRad
+    s = (math.Pi * r * i)/2
+    xc = r * math.Cos(ang)
+    yc = r * math.Sin(ang)
+    px = append(px, int(xc+(s * math.Sin(ang))))
+    py = append(py, int(yc-(s * math.Cos(ang))))
   }
-  for i := 0; i < width - cx; i += spaceing {
-    canvas.Line(cx+i, 0, cx+i, height, gridStyle)
-    canvas.Line(cx-i, 0, cx-i, height, gridStyle)
-  }
-  canvas.Gend()
+  canvas.Polyline(px, py, solidStyle)
 }
 
 func plotGear(cx int, cy int, rot float64, g gear.Gear, canvas *svg.SVG){
   canvas.Gtransform(fmt.Sprintf("translate(%d, %d)", cx, cy))
-  canvas.Circle(0, 0, int(g.GetOutsideDia() * 100 / 2), solidStyle)
-  canvas.Circle(0, 0, int(g.Pd * 100 / 2), dashStyle)
-  canvas.Circle(0, 0, int(g.GetRootCircleDia() * 100 / 2), dashStyle)
-  cntrLen := int(g.GetOutsideDia() * 100 / 8)
-  canvas.Line( -cntrLen, 0, cntrLen, 0, thinStyle)
-  canvas.Line(0, -cntrLen, 0, cntrLen, thinStyle)
+  canvas.Circle(0, 0, int(g.GetOutsideDia() * factor / 2), solidStyle)
+  canvas.Circle(0, 0, int(g.Pd * factor / 2), dashStyle)
+  canvas.Circle(0, 0, int(g.GetRootCircleDia() * factor / 2), dashStyle)
+  canvas.Circle(0, 0, int(g.GetBaseCircleDia() * factor / 2), thinStyle)
+  cntrLen := int(g.GetOutsideDia() * factor / 8)
+  canvas.Line( -cntrLen, 0, cntrLen, 0, solidStyle)
+  canvas.Line(0, -cntrLen, 0, cntrLen, solidStyle)
   canvas.Gtransform(fmt.Sprintf("rotate(%0.3f)", rot))
   for i := 0; i<g.N; i++ {
-    canvas.Line(int((math.Cos((360/float64(g.N))*float64(i)*DegToRad)*g.GetRootCircleDia()*50)),
-                int((math.Sin((360/float64(g.N))*float64(i)*DegToRad)*g.GetRootCircleDia()*50)),
-                int((math.Cos((360/float64(g.N))*float64(i)*DegToRad)*g.GetOutsideDia()*50)),
-                int((math.Sin((360/float64(g.N))*float64(i)*DegToRad)*g.GetOutsideDia()*50)),
+    canvas.Line(int((math.Cos((360/float64(g.N))*float64(i)*DegToRad)*g.GetRootCircleDia()*factor/2)),
+                int((math.Sin((360/float64(g.N))*float64(i)*DegToRad)*g.GetRootCircleDia()*factor/2)),
+                int((math.Cos((360/float64(g.N))*float64(i)*DegToRad)*g.GetOutsideDia()*factor/2)),
+                int((math.Sin((360/float64(g.N))*float64(i)*DegToRad)*g.GetOutsideDia()*factor/2)),
                 thinStyle)
+    canvas.Gtransform(fmt.Sprintf("rotate(%f)", 360.0/float64(g.N)*float64(i)))
+    plotInvCurve(g, canvas)
+    canvas.Gend()
   }
   canvas.Gend()
   canvas.Gend()
@@ -73,8 +93,8 @@ func Plot(g1, g2 gear.Gear) {
 
 
   centerDist := (g1.Pd + g2.Pd) / 2
-  cx := int((border + (g1.GetOutsideDia() / 2.0)) * 100)
-  cy := height * 50
+  cx := int((border + (g1.GetOutsideDia() / 2.0)) * factor)
+  cy := height * factor / 2
   f, err := os.Create("TestFile.svg")
   if err != nil {
     fmt.Println("Something failed creating file!")
@@ -82,11 +102,11 @@ func Plot(g1, g2 gear.Gear) {
   canvas := svg.New(f)
 
   // Setup canvas so that each drawing unit is 0.01mm.
-  canvas.StartviewUnit(width, height, "mm", 0, 0, width * 100, height * 100)
-  plotGrid(cx, cy, width * 100, height * 100, canvas)
+  canvas.StartviewUnit(width, height, "mm", 0, 0, width * factor, height * factor)
+  plotGrid(cx, cy, width * factor, height * factor, canvas)
   rot := 0.0
   plotGear(cx, cy, rot, g1, canvas)
-  cx = cx + int(centerDist * 100)
+  cx = cx + int(centerDist * factor)
   if math.Mod(float64(g2.N), 2) == 0{
     rot = 180.0 / float64(g2.N)
   }
