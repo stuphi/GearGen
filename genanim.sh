fc92e230519bacc843bfa6866646963582e1b31e
stuphi <philip@stuphi.co.uk>
2015-10-02 11:44:11 +0100

    Remove parrallel from animation script as not available in Cygwin

--- genanim.sh
+++ genanim.sh
@@ -16,13 +16,21 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+function gencon {
+  f=`printf "anim_%02d" $1`
+  echo "Working on frame $1"
+  GearGen -o $f -r $1 -n1 17 -n2 23
+  inkscape --without-gui --file $f.svg --export-dpi=150 --export-png=$f.png &>/dev/null
+  convert $f.png -flatten -resize 50% $f.png
+  rm $f.svg
+}
 
-seq 0 99 | parallel 'f=`printf "anim_%02d" {}`;
-  echo "Working on frame {}";
-  GearGen -o $f -r {} -n1 17 -n2 23;
-  inkscape --without-gui --file $f.svg --export-dpi=150 --export-png=$f.png &>/dev/null;
-  convert $f.png -flatten -resize 50% $f.png;
-  rm $f.svg'
+for ((i=0; i<100; i++))
+do
+  gencon $i &
+done
+echo "Waiting for jobs to complete."
+wait
 echo "Now make animation..."
 convert -delay 5 -loop 0 anim*.png animation.gif
 rm anim_??.png
