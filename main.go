14783296c6f284d2103a0eadd7e9fd0e66e3a0e8
stuphi <philip@stuphi.co.uk>
2015-10-03 20:26:43 +0100

    Add a quick comment to the main function so that godoc can see somthing useful on the top level.

--- main.go
+++ main.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// GearGen will produce an SVG image of a pair of meshing gears. Run the command
+// with the -h option to get usage details.
 package main
 
 import (
